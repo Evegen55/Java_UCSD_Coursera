4461c3cd1adfd0c8a68a0d41f7aaf25a74f0ce71
Evegenn <demerbrise@gmail.com>
2015-11-22 22:04:25 +0300

    DictionaryBST has been completed, Online Grader told that result 6/6

--- MOOCTextEditor/src/spelling/DictionaryBST.java
+++ MOOCTextEditor/src/spelling/DictionaryBST.java
@@ -1,5 +1,6 @@
 package spelling;
 
+import java.util.LinkedList;
 import java.util.TreeSet;
 
 /**
@@ -12,7 +13,11 @@ public class DictionaryBST implements Dictionary
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
-	
+	public DictionaryBST() {
+		
+		this.dict = new TreeSet<String>();
+				
+	}	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
@@ -21,7 +26,14 @@ public class DictionaryBST implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
-        return false;
+    	if(isWord(word)) {
+    		
+    		return false;
+    		
+    	} else 
+    	
+    	return dict.add(word.toLowerCase());
+    
     }
 
 
@@ -29,13 +41,17 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
     	//TODO: Implement this method
-        return false;
+    	if(dict.contains(s.toLowerCase())) {
+            return true;
+        }else {
+        	return false;
+        }
     }
 
 }
